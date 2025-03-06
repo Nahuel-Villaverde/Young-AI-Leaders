df57176745d0b20e42be34ca3ed7179c507c73b4
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2025-03-06 20:29:52 -0300

    comunidad terminado

--- src/components/Comunidad.jsx
+++ src/components/Comunidad.jsx
@@ -2,9 +2,52 @@ import './Comunidad.css';
 
 function Comunidad() {
     return (
-      <section className="section" id="comunidad">
-        <h2>Comunidad</h2>
-      </section>
+      <section className="comunidad-section" id="comunidad">
+       <div className="grid-container">
+        <div className="grid-item">
+          <h2>Oportunidades de Crecimiento</h2>
+          <p>Al participar, accederás a mentorías, capacitaciones y eventos exclusivos que fortalecerán tus habilidades técnicas y de liderazgo en IA para el bien social.</p>
+        </div>
+        <div className="grid-item">
+          <h2>Formar Parte de la Comunidad</h2>
+          <p>Si eres un joven entusiasta de la inteligencia artificial con ganas de generar impacto social, puedes unirte a nuestra red global y colaborar en proyectos innovadores alineados con los ODS de la ONU.</p>
+        </div>
+        <div className="grid-item">
+          <span className='ul-title'>Requisitos</span>
+          <ul className="stats-list">
+            <li>Asistir a reuniones mensuales.</li>
+            <li>Participar en al menos un proyecto específi co del centro por año.</li>
+            <li>Participar en al menos 1 proyecto con AI for Good por año.</li>
+            <li>Colabora con al menos otro centro, dentro o fuera de tu región.</li>
+            <li>Contribuir a la visión de la comunidad de aprovechar la IA para el bien social.</li>
+            <li>Respetar los estatutos y reglas de la comunidad.</li>
+            <li>Asegúrese de que todas sus acciones y proyectos se centren en cómo la IA puede ayudar a apoyar los ODS de las Naciones Unidas.</li>
+          </ul>
+        </div>
+        <div className="grid-item">
+          <div className="stats-metrics">
+            <div className="metric">
+              <span className="stat-number">+20</span>
+              <span className="stat-label">Centros Activos</span>
+            </div>
+            <div className="metric">
+              <span className="stat-number">+12</span>
+              <span className="stat-label">Países Representados</span>
+            </div>
+            <div className="metric">
+              <span className="stat-number">+300</span>
+              <span className="stat-label">Proyectos Desarrollados</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='buttons-comnidad-container'>
+        <button className='button-comnidad-1'>Únete a la comunidad</button>
+        <button className='button-comnidad-2'>Explorar proyectos</button>
+      </div>
+    </section>
     );
   }
   
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,34 @@
 import { Link } from 'react-scroll';
+import { useEffect, useState } from "react";
 import './Header.css';
 
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className={`header ${scrolled ? "scrolled" : ""}`}>
       <Link to="home" smooth={true} duration={500} offset={-200} className="logo-container">
         <img src="/Logo-YoungAi.png" alt="Logo Young Ai" />
       </Link>
       <nav>
         <ul className="nav-links">
           <li><Link to="aboutUs" smooth={true} duration={500} offset={-80} className="nav-item">About Us</Link></li>
-          <li><Link to="comunidad" smooth={true} duration={500} offset={-80} className="nav-item">Comunidad</Link></li>
+          <li><Link to="comunidad" smooth={true} duration={500} className="nav-item">Comunidad</Link></li>
           <li><Link to="proyectos" smooth={true} duration={500} offset={-80} className="nav-item">Proyectos</Link></li>
           <li><Link to="contacto" smooth={true} duration={500} offset={-80} className="nav-item">Contacto</Link></li>
         </ul>
