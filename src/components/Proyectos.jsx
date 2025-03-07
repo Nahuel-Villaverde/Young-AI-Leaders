75df3a999c287fe4b6d2005beaa276e0ccc5bd6d
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2025-03-07 20:57:32 -0300

    proyectos

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,33 +1,26 @@
+import './Proyectos.css';
+
 function Proyectos() {
-    return (
-      <section className="section" id="proyectos">
-        <h2>Proyectos</h2>
-        <p>Empresas y emprendedores que han confiado en nosotros.</p>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </section>
-    );
-  }
-  
-  export default Proyectos;
-  
+  return (
+    <section className="proyectos-section" id="proyectos">
+      <div className='proyect-section-container'>
+        <h2 className='proyectos-titulo-section'>Proyectos destacados</h2>
+        <div className='proyects-container'>
+          <div className='proyect'>
+            <img src="/proyect1.png" alt="proyecto 1" />
+            <h3>AI Learning Hub</h3>
+            <p>Una plataforma educativa impulsada por IA que ayuda a jóvenes a aprender sobre inteligencia artificial a su propio ritmo. Con recursos interactivos, ejercicios prácticos y mentorías, este proyecto busca democratizar el acceso a la educación en IA.</p>
+          </div>
+
+          <div className='proyect'>
+            <img src="/proyect1.png" alt="proyecto 1" />
+            <h3>AI Learning Hub</h3>
+            <p>Una plataforma educativa impulsada por IA que ayuda a jóvenes a aprender sobre inteligencia artificial a su propio ritmo. Con recursos interactivos, ejercicios prácticos y mentorías, este proyecto busca democratizar el acceso a la educación en IA.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Proyectos;
