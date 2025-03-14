ac1fe25d379edd6308456be348de72ac5e80aeab
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2025-03-14 20:34:48 -0300

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,18 @@
 import { Link } from 'react-scroll';
 import { useEffect, useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 import './Header.css';
 
 function Header() {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
+      setScrolled(window.scrollY > 50);
     };
-
     window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
@@ -25,14 +20,17 @@ function Header() {
       <Link to="home" smooth={true} duration={500} offset={-200} className="logo-container">
         <img src="/Buenos-Aires-Hub-Logo.webp" alt="Logo Young Ai" />
       </Link>
-      <nav>
+      <nav className={`nav ${menuOpen ? "open" : ""}`}>
         <ul className="nav-links">
-          <li><Link to="aboutUs" smooth={true} duration={500} offset={-80} className="nav-item">About Us</Link></li>
-          <li><Link to="comunidad" smooth={true} duration={500} className="nav-item">Comunidad</Link></li>
-          <li><Link to="proyectos" smooth={true} duration={500}  className="nav-item">Proyectos</Link></li>
-          <li><Link to="contacto" smooth={true} duration={500} className="nav-item">Contacto</Link></li>
+          <li><Link to="aboutUs" smooth={true} duration={500} offset={-80} className="nav-item" onClick={() => setMenuOpen(false)}>About Us</Link></li>
+          <li><Link to="comunidad" smooth={true} duration={500} className="nav-item" onClick={() => setMenuOpen(false)}>Comunidad</Link></li>
+          <li><Link to="proyectos" smooth={true} duration={500}  className="nav-item" onClick={() => setMenuOpen(false)}>Proyectos</Link></li>
+          <li><Link to="contacto" smooth={true} duration={500} className="nav-item" onClick={() => setMenuOpen(false)}>Contacto</Link></li>
         </ul>
       </nav>
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </div>
     </header>
   );
 }
