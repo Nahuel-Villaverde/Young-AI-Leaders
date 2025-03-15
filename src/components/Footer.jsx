a287780499945cd908bb805754c746abc7d5aaec
Yago Zaragoza <yagozaragoza04@gmail.com>
2025-03-15 18:12:49 -0300

    Footer Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,29 @@
+import "./Footer.css";
+
 function Footer() {
-    return (
-      <footer className="footer">
-        <p>© 2025 Yana. Todos los derechos reservados.</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
-  
+  return (
+    <footer className="footer">
+      <img src="/Afondo.png" alt="Decoración" className="fondo-imagen" />
+
+      <div className="container_display_grid">
+        <div className="area_1">Área 1</div>
+        <div className="area_2">Área 2</div>
+        <div className="area_3">
+          <div class="linea-div3"></div>
+        </div>
+        <div className="area_4">
+          <p className="derechos">
+            © 2025 Young AI Leaders. Todos los derechos reservados.
+          </p>
+        </div>
+        <div className="area_5">
+          <a href="#" rel="noopener noreferrer">
+            <p className="termino_y_condiciones">Términos y Condiciones</p>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
